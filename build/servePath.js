49398e38c840a05fb7f8a03e81c2e865689d7c35
zhanght <983033995@qq.com>
2020-11-04 19:24:34 +0800

    打包地址修改

--- build/servePath.js
+++ build/servePath.js
@@ -2,12 +2,12 @@
  * @Description:
  * @Author: zhangHeTeng
  * @Date: 2020-09-27 10:20:20
- * @LastEditTime: 2020-11-04 15:19:32
+ * @LastEditTime: 2020-11-04 19:23:45
  * @LastEditors: zhangHeTeng
  */
 module.exports = {
   dev: "https://5818222.com", // 本地调试地址
   serve: "http://test.zhangheteng.com:3000", // 线上地址
   // build: "http://www.zhangheteng.com/api"
-  build: "http://blog.zhangheteng.com:3000/"
+  build: "https://5818222.com"
 };
